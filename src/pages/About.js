4cbac870213170f88702253f22b93436a878447a
lillian-tran <lilliantran17@gmail.com>
2024-06-05 22:30:48 -0400

    additional about updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,8 +18,14 @@ export default class About extends Component {
         <div className="centered">
         <div className="name_title">Lillian Tran</div>
         <div className="brief_description">
-          Hi! My name is Lillian and I'm a born and raised New Yorker 
-          currently studying computer science @ Tufts. 
+          Hi - nice to meet you, my name is Lillian! I'm a born and raised 
+          New Yorker currently studying computer science @ Tufts (as a rising 
+          junior). 
+          My interests include cafe hopping and exploring museums! A fun 
+          fact about me is that I don't have my driver's license yet (but I'm 
+          working on it)! 
+          <br></br>
+          :) 
         </div>
         </div>
         </div>
