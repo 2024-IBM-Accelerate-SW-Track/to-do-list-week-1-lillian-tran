901f87e0597953a9899368d6a157bfee0e724667
lillian-tran <lilliantran17@gmail.com>
2024-06-05 21:47:37 -0400

    updated about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
+import "./About.css";
+import lillianImage from "../assets/lillian.jpg";
+
 import React, { Component } from 'react';
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
-      </div>
+        <div className="split left">
+          <div className="centered">
+          <img className="profile_image"
+            src={lillianImage}
+          alt="Profile Pic"></img>
+        </div>
+        </div>
+        <div className="split right">
+        <div className="centered">
+        <div className="name_title">Lillian Tran</div>
+        <div className="brief_description">
+          Hi! My name is Lillian and I'm a born and raised New Yorker 
+          currently studying computer science @ Tufts. 
+        </div>
+        </div>
+        </div>
+        </div>
     )
   }
 }
